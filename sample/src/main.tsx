0e21b43932acae36fa1fed9b01b019e1c208720f
kuramapommel <pommel.pg@gmail.com>
2024-08-14 22:51:42 +0900

    react-admin のページを組み込んでみたけど、create ボタン押下時のエラーが解消できていない

--- sample/src/main.tsx
+++ sample/src/main.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import Login from '@/pages/login'
 import ProductList from '@/pages/product-list'
 import App from '@/App'
 import { enableMocking } from '@/testing/mocks'
+import Tenant from './pages/tenant'
 
 enableMocking().then(() =>
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/login" element={<Login />} />
           <Route path="/products" element={<ProductList />} />
+          <Route path="/tenant/*" element={<Tenant />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </React.StrictMode>,
   ),
 )
---
+++ sample/src/pages/tenant.tsx
@@ -0,0 +1,126 @@
+import {
+  Admin,
+  Create,
+  CreateResult,
+  Datagrid,
+  DataProvider,
+  DeleteResult,
+  Edit,
+  EditButton,
+  GetOneResult,
+  List,
+  RaRecord,
+  Resource,
+  SimpleForm,
+  TextField,
+  TextInput,
+  UpdateResult,
+} from 'react-admin'
+
+type Post = {
+  id: string
+  title: string
+  body: string
+} & RaRecord<string>
+
+let posts: Post[] = []
+
+const dataProvider: DataProvider = {
+  create: (_resource, params) => {
+    const { title, body } = params.data
+    if (!title || !body) return Promise.resolve<CreateResult>({ data: null })
+
+    const post = { id: String(posts.length + 1), title, body }
+    posts = [...posts, post]
+    return Promise.resolve<CreateResult>({
+      data: post,
+    })
+  },
+  delete: (_resource, params) => {
+    const post = posts.find((p) => p.id === params.id)
+    if (!post) return Promise.resolve<DeleteResult>({ data: null })
+
+    posts = posts.filter((p) => p.id !== post.id)
+    return Promise.resolve<DeleteResult>({ data: post })
+  },
+  deleteMany: (_resource, params) => {
+    posts = posts.filter((p) => !params.ids.includes(p.id))
+    return Promise.resolve({ data: posts.map((p) => p.id) })
+  },
+  getList: () => Promise.resolve({ data: posts as [], total: posts.length }),
+  getMany: (_resource, params) =>
+    Promise.resolve({
+      data: posts.filter((p) => params.ids.includes(p.id)) as [],
+    }),
+  getManyReference: () =>
+    Promise.resolve({ data: posts as [], total: posts.length }),
+  getOne: (_resource, params) =>
+    Promise.resolve<GetOneResult>({
+      data: posts.find((p) => p.id === params.id) || null,
+    }),
+  update: (_resource, params) => {
+    const post = posts.find((p) => p.id === params.id)
+    if (!post) return Promise.resolve<UpdateResult>({ data: null })
+
+    const updated = { ...post, ...params.data }
+
+    posts = posts.map((p) => (p.id === params.id ? updated : p))
+    return Promise.resolve<UpdateResult>({ data: updated })
+  },
+  updateMany: (_resource, params) => {
+    const targets = posts.filter((p) => params.ids.includes(p.id))
+    if (!targets.length) return Promise.resolve({ data: [] })
+
+    const updateds = targets.map((p) => ({ ...p, ...params.data }))
+    posts = posts.map((p) => {
+      const updated = updateds.find((u) => u.id === p.id)
+      return updated || p
+    })
+    return Promise.resolve({ data: updateds as [] })
+  },
+}
+
+const PostList = () => (
+  <List>
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="title" />
+      <TextField source="body" />
+      <EditButton />
+    </Datagrid>
+  </List>
+)
+
+export const PostEdit = () => (
+  <Edit title="Edit a Post">
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="title" />
+      <TextInput source="body" />
+    </SimpleForm>
+  </Edit>
+)
+
+const PostCreate = () => (
+  <Create title="Create a Post">
+    <SimpleForm>
+      <TextInput source="title" />
+      <TextInput source="body" />
+    </SimpleForm>
+  </Create>
+)
+
+const Tenant: React.FC = () => {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="posts"
+        list={PostList}
+        edit={PostEdit}
+        create={PostCreate}
+      />
+    </Admin>
+  )
+}
+
+export default Tenant
