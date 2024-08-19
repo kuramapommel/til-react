6c1e171a0410b213909e9ece5076db4e8022b192
kuramapommel <pommel.pg@gmail.com>
2024-08-20 00:07:45 +0900

    iterator の key 指定をより妥当な値に修正

--- sample/src/components/organisms/time-list-table.tsx
+++ sample/src/components/organisms/time-list-table.tsx
@@ -22,23 +22,23 @@ const TimeListTable = React.memo(function TimeListTable(props: Props) {
       <thead>
         <tr>
           <th className="min-w-28">ルーム</th>
-          {[...range(0, 5)].map((num, index) => (
-            <th key={index} className="min-w-28 text-left">
+          {[...range(0, 5)].map((num) => (
+            <th key={num} className="min-w-28 text-left">
               {String(num).padStart(2, '0')}:00
             </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {props.rooms.map((room, index) => (
-          <tr key={index}>
+        {props.rooms.map((room) => (
+          <tr key={room.id}>
             <td className="min-w-28 text-center border-2 border-white text-xl">
               {room.name}
             </td>
-            {room.availables.map((available, availableIndex) => {
+            {room.availables.map((available, index) => {
               return (
                 <td
-                  key={availableIndex}
+                  key={index}
                   className={`min-w-28 ${available ? 'bg-green-400' : 'bg-gray-400'} border-2 border-white `}
                   onClick={
                     available
