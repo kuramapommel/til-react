006e8a885866920a888318ab76066b49cf909a31
kuramapommel <pommel.pg@gmail.com>
2024-08-24 23:54:38 +0900

    storybook 起動時に mock service worker を起動するように修正

--- sample/.storybook/preview.ts
+++ sample/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react'
+import { initialize, mswLoader } from 'msw-storybook-addon'
 import '../src/index.css'
+import { handlers } from '../src/testing/mocks/handlers'
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +11,15 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    msw: {
+      handlers: [...handlers],
+    },
   },
+  // Provide the MSW addon loader globally
+  loaders: [mswLoader],
 }
+
+// Initialize MSW
+initialize()
 
 export default preview
